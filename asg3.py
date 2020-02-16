3ef8c6a2b8de9c9a0a4f0eab35a9bbbeedb520cf
Ziw96 <ziwei.ricky.li@foxmail.com>
2020-02-16 15:12:51 -0500

    update import method in asg3.py, add one more case in func_test.py

--- asg3.py
+++ asg3.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 """NLP Preprocessing Library"""
-
-from nltk.tokenize import TweetTokenizer
-
+import nltk.tokenize
 def clean_text(raw_text):
     """Remove url, tokens"""
     raw_text = raw_text.replace('RT ', '')
@@ -17,7 +15,7 @@ def clean_text(raw_text):
 
 def tokenize_text(tweet_str):
     """convert string to chunks of text"""
-    tokenizer = TweetTokenizer(strip_handles=True, reduce_len=True)
+    tokenizer = nltk.tokenize.TweetTokenizer(strip_handles=True, reduce_len=True)
     return tokenizer.tokenize(tweet_str)
     # Source: https://www.nltk.org/api/nltk.tokenize.html
 
